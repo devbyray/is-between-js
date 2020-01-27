c1fc35978ccf997754b27c58d378049596e729e4
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2020-01-27 21:57:00 +0100

    Rebuild function

--- test/is-between-js.test.ts
+++ test/is-between-js.test.ts
@@ -1,24 +1,59 @@
-import isBetween from '../src/is-between-js'
+import { between } from '../src/is-between-js'
 
 /**
  * Dummy test
  */
 describe('isBetween test', () => {
   it('works if isBetween is truthy', () => {
-    expect(isBetween).toBeDefined()
+    expect(between).toBeDefined()
   })
 
   it('isBetween returns true', () => {
-    expect(isBetween(10, 0, 100)).toBe(true)
-    expect(isBetween(1, 0, 10)).toBe(true)
-    expect(isBetween(-5, -10, 10)).toBe(true)
-    expect(isBetween(1.2, 0, 2)).toBe(true)
-    expect(isBetween(3.52, 3.52, 3.53)).toBe(true)
+    expect(
+      between(10)
+        .min(0)
+        .max(100)
+        .calc()
+    ).toBe(true)
+    expect(
+      between(1)
+        .min(0)
+        .max(10)
+        .calc()
+    ).toBe(true)
+    expect(
+      between(-5)
+        .min(-10)
+        .max(10)
+        .calc()
+    ).toBe(true)
+    expect(
+      between(1.2)
+        .min(0)
+        .max(2)
+        .calc()
+    ).toBe(true)
+    expect(
+      between(3.52)
+        .min(3.52)
+        .max(3.53)
+        .calc()
+    ).toBe(true)
   })
   it('isBetween returns false', () => {
-    expect(isBetween(10, 0, 9)).toBe(false)
+    expect(
+      between(10)
+        .min(0)
+        .max(9)
+        .calc()
+    ).toBe(false)
   })
   it('isBetween returns error', () => {
-    expect(() => isBetween(10, 15, 9)).toThrow()
+    expect(() =>
+      between(10)
+        .min(15)
+        .max(9)
+        .calc()
+    ).toThrow()
   })
 })
